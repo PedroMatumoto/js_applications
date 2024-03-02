0f056ebf424ad0c9e1fd3fecdb4bb2eb6afb6a80
Pedro Matumoto <pedromatumoto@gmail.com>
2024-03-02 09:15:27 -0300

    Início projeto e conceitos de javascript

---
+++ aula1/index.js
@@ -0,0 +1,76 @@
+
+
+/* 
+A LINGUAGEM TEM TIPAÇÃO FRACA E DINÂMICA 
+- Fraca: não é necessário declarar o tipo de dado
+- Dinâmica: o tipo de dado pode mudar em tempo de execução
+- Transpilação: conversão de um código de uma linguagem para outra
+- JIT: Just in Time
+- Compila em tempo de execuçãos
+*/
+
+/*
+//// const, let e var 
+const nome = "José"
+const sobrenome = 'Silva'
+const nomeFinal = 'Mendes'
+// Não é possível alterar o valor de uma constante
+// nome = "Ana"
+
+// let de 'ser'
+let idade = 19
+idade = 20
+console.log(nome)
+console.log(idade)
+*/
+
+/*
+var linguagem = "JavaScript"
+console.log(`Aprendendo ${linguagem}`)
+var linguagem = "Java"
+console.log(`Aprendendo ${linguagem}`)
+*/
+
+/*
+var idade = 19
+console.log(`Oi,${nome}!`)
+
+if (idade >= 18) {
+    var  nome = "José" //içamento (hoisting)
+    console.log(`Maior de idade ${nome}!`)
+}
+console.log(`Até mais, ${nome}!`)
+*/
+
+/*
+// coerção implícita
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + Number(n2)
+console.log(n3)
+*/
+
+/*
+console.log(1==1)
+console.log(1=='1')
+console.log(1==='1')
+// != !==
+console.log('1'==='1')
+*/
+
+/*
+console.log(true==1)
+console.log(1==[1])
+console.log(null==null)
+console.log(undefined==undefined)
+console.log(undefined==null)
+*/
+
+console.log([]==[])
+
+
+
+
+
+
+
